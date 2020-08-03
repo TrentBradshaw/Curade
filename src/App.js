3a16a33296aa6ca943fda178c3e0fefe83e132e1
Trent Bradshaw <developerwalrus@gmail.com>
2020-08-03 14:08:39 -0700

    reactadded

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from './Navbar'
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      <h1>HELLO</h1>
+      
+    </div>
+    
+  );
+}
+
+export default App;
---
+++ src/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+function Navbar(){
+    return(
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>Explore</li>
+                <li>Notifications</li>
+                <li>Messages</li>
+                <li>Liked</li>
+                <li>Profile</li>
+                <li>Settings</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar;
