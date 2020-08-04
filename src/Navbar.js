2ce78b8f313d5a9b902c38e621ded3035433af08
Trent Bradshaw <developerwalrus@gmail.com>
2020-08-04 15:15:44 -0700

    navbar changes

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,21 +3,15 @@ import './Navbar.css';
 function Navbar(){
     return(
         <nav className="Navbar" id="Navbar">
-            <ul id="LeftNavbar">
-                    <li>Home</li>
-                    <li>Explore</li>
-                    <li>Notifications</li>
-                    <li>Messages</li>
-            </ul>
+            
+            <a href='curade.com'>Home</a>
+            <a href='curade.com/explore'>Explore</a>
+            <a href='curade.com/notifications'>Notifications</a>
             <h1 id="CuradeHeader">CURADE</h1>
-            <ul id="RightNavbar">
-                <li>Liked</li>
-                <li>Profile</li>
-                <li>Settings</li>
-            </ul>
-        
+            
+            <a>Profile</a>
+            <a>Settings</a>
         </nav>
-    )
-}
+)}
 
 export default Navbar;
